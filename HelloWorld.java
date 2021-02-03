eaa20d35587e47ce98099717b5a3e11f234adb8d
Cody S <cody.s.snow@gmail.com>
2021-02-03 10:07:10 -0500

    added Goodbye message

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main (String args[]){
     	System.out.println("Hear ye: important announcement!");
         System.out.println("Hello, world!");
+        System.out.println("Fair thee well, world!");
     }
 }
