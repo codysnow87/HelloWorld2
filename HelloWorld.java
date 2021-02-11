4a706bd88f5f696e34f7f8cbf8874d656dd57d94
Cody S <cody.s.snow@gmail.com>
2021-02-10 23:06:52 -0500

    Closes #2

--- HelloWorld.java
+++ HelloWorld.java
@@ -13,5 +13,23 @@ public class HelloWorld {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
         LocalDateTime now = LocalDateTime.now();  
         System.out.println(dtf.format(now));  
+        
+        /* Total number of processors or cores available to the JVM */
+        System.out.println("Available processors (cores): " + 
+        Runtime.getRuntime().availableProcessors());
+
+        /* Total amount of free memory available to the JVM */
+        System.out.println("Free memory (bytes): " + 
+        Runtime.getRuntime().freeMemory());
+
+        /* This will return Long.MAX_VALUE if there is no preset limit */
+        long maxMemory = Runtime.getRuntime().maxMemory();
+        /* Maximum amount of memory the JVM will attempt to use */
+        System.out.println("Maximum memory (bytes): " + 
+        (maxMemory == Long.MAX_VALUE ? "no limit" : maxMemory));
+
+        /* Total memory currently in use by the JVM */
+        System.out.println("Total memory (bytes): " + 
+        Runtime.getRuntime().totalMemory());
     }
 }
